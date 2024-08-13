c48c4f493cbbcb4d1f8c852757eedeaf5ead56ac
Patrick Durojaiye <pt.durojaiye@gmail.com>
2024-08-13 02:35:12 +0100

    Updated Brent Method Calibration

--- volatility_modelling/calibration_methods/bent_calibration.py
+++ volatility_modelling/calibration_methods/bent_calibration.py
@@ -1,5 +1,5 @@
 from trader.PricingModel import BlackScholes
-from scipy.optimize import brent, minimize_scalar
+from scipy.optimize import brent
 
 class BrentMethod:
     """
@@ -19,7 +19,7 @@ class BrentMethod:
         self.tolerance = tolerance
         self.bs = BlackScholes()
 
-    def brent_solution(self, spot_price: float, strike_price: float, r: float, t: float, market_price: float, moneyness: float):
+    def brent_solution(self, spot_price: float, strike_price: float, r: float, t: float, market_price: float, moneyness: float, put_price):
         """
         Finds the implied volatility using Brent's method.
 
@@ -30,32 +30,39 @@ class BrentMethod:
         t (float): The time to maturity of the option.
         market_price (float): The market price of the option.
         moneyness (float): The moneyness of the option (relative measure of how close the option is to being in the money).
+        put_price (float): Price of put option for respective strike
 
         Returns:
         float: The implied volatility.
         """
 
-        def objective_function(sigma):
+        def objective_function(sigma, option_type):
             """
             Objective function to find the root for implied volatility.
 
             Parameters:
             sigma (float): The volatility to test.
+            option_type (str): Either Put or Call, denotes which type of contract we are calibrating to
 
             Returns:
             float: The absolute difference between the Black-Scholes price and the market price.
             """
+
+            if option_type == "Put":
+                return abs(self.bs.put_value(spot_price=spot_price, strike_price=strike_price, r=r, sigma=sigma, t=t) - put_price)
             return abs(self.bs.call_value(spot_price=spot_price, strike_price=strike_price, r=r, sigma=sigma, t=t) - market_price)
 
-        # Define initial bounds based on log moneyness log(K/S)
-        if moneyness < -0.25:
-            lower_bound = 0.98
-            upper_bound = 20
+        # Define initial bounds and option to calibrate to based on log moneyness log(K/S)
+        if moneyness < 0:
+            lower_bound = 0.4
+            upper_bound = 3
+            option_type = "Put"
 
         else:
-            lower_bound = 1e-6
+            lower_bound = 0.4
             upper_bound = 3
+            option_type = "Call"
 
-        implied_volatility = brent(func=objective_function, brack=(lower_bound, upper_bound), tol=1e-8, maxiter=50)
+        implied_volatility = brent(func=objective_function, brack=(lower_bound, upper_bound), tol=1e-7, maxiter=500, args=(option_type, ))
 
         return implied_volatility
