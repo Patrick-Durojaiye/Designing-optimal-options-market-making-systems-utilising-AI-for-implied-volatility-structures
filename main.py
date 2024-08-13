3eba044a45f414c93ac199edd12073a74a6a638f
Patrick Durojaiye <pt.durojaiye@gmail.com>
2024-08-13 07:39:56 +0100

    Saved ConvLSTM model upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from scripts.run_svi_calibration import run_svi_calibration
 from testing.volatility_modelling.bent_tester import test_brent_method
 from testing.volatility_modelling.newton_raphson_tester import test_newton_raphson
-from testing.volatility_modelling.svi_second_method_tester import test_svi_second_method_calibration
 from testing.volatility_modelling.svi_tester import test_svi_calibration
 from volatility_modelling.utilities.vega_analysis import plot_vegas
 
@@ -9,7 +8,6 @@ from volatility_modelling.utilities.vega_analysis import plot_vegas
 def main():
     # plot_vegas()
     # test_svi_calibration()
-    # test_svi_second_method_calibration()
     # test_newton_raphson(1.0)
     # test_brent_method()
     run_svi_calibration()
